44babaa0e902b3ddc7a853a532b250fd524bf9f6
Eddie Gonzalez <eddie_gonzalez@Eddies-MacBook-Pro.local>
2022-04-30 11:33:43 -0700

    adding updates to battleship.js;

--- battleship.js
+++ battleship.js
@@ -1,6 +1,6 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var location1 = 2;
+var location2 = 3;
+var location3 = 4;
 
 var guess;
 var hits = 0;
@@ -9,7 +9,7 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
-    guess = prompt("Ready, aim, fire! (enter a number from 0 - 6): ");
+    guess = prompt("Ready, aim, fire! (enter a number from 0 - 6) you butthead!: ");
 
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
@@ -21,10 +21,12 @@ while (isSunk == false) {
             
             if (hits == 3) {
                 isSunk = true;
-                alert("You sank my battleship!!")
+                alert("You sank my battleship you jerk!!");
             } else {
-                alert("MISS!")
+                alert("You hit me!!!");
             }
+        } else {
+            alert("MISS!");
         }
     }
 }
