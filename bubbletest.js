263de1cac768218c497e256695b845be35fea05f
Eddie Gonzalez <eddie_gonzalez@Eddies-MBP.fios-router.home>
2022-05-22 14:38:42 -0700

    update to bubbletest.js added costs array

--- bubbletest.js
+++ bubbletest.js
@@ -6,8 +6,14 @@ function bubbleTest() {
                   46, 31, 57, 52, 44, 18,
                   41, 53, 90, 88, 51, 44];
 
+    var costs = [.25, .27, .25, .25, .25 ,.25,
+                 .33, .31, .25, .29, .27, .22,
+                 .31, .25, .25, .33, .21, .25,
+                 .25, .25, .28, .25, .24, .22,
+                 .20, .25, .30, .25, .24, .25,
+                 .25, .25, .27, .25, .26, .29];
+
     function printAndGetScores(scores) {
-        
         var highScore = 0;
         var output;
 
@@ -33,10 +39,28 @@ function bubbleTest() {
         return bestSolutions;
     }
 
+    function getMostCostEffectiveSolution(scores, costs, highScore) {
+        var cost = 100;
+        var index;
+
+        for (var i = 0; i < scores.length; i++) {
+            if (scores[i] == highScore) {
+                if (cost > costs[i]) {
+                    cost = costs[i];
+                    index = i;
+                }
+            }
+        }
+        return index;
+    }
+
     var highScore = printAndGetScores(scores);
+    var bestSolutions = getBestSolutions(scores, highScore);
+    var mostCostEffectiveSolution = getMostCostEffectiveSolution(scores, costs, highScore) + 1;
     console.log("Number of Bubble Tests: " + scores.length);
     console.log("The high bubble score is: " + highScore);
-    console.log("The tests with the high score are: " + getBestSolutions(scores, highScore));
+    console.log("The tests with the high score are: " + bestSolutions);
+    console.log("The bubble solution that was most cost effective was #" + mostCostEffectiveSolution);
 }
 
 bubbleTest();
