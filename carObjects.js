c989080e93036800702cdffa9ee6b988c3307f2f
Eddie Gonzalez <eddie_gonzalez@Eddies-MBP.fios-router.home>
2022-05-22 15:30:16 -0700

    added carObjects.js

---
+++ carObjects.js
@@ -0,0 +1,19 @@
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+};
+
+var cadi = {
+    make: "Cadillac",
+    model: "CTS-V",
+    year: 2023,
+    color: "black",
+    passengers: 5,
+    convertible: false,
+    mileage: 5
+}
