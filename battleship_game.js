251be5d9ba47699548999d835fa0a8100ce653da
Eddie Gonzalez <eddie_gonzalez@Eddies-MBP.fios-router.home>
2022-05-31 16:37:58 -0700

    added updates to model object

--- battleship_game.js
+++ battleship_game.js
@@ -18,10 +18,10 @@ var view = {
 }; 
 
 var model = {
-    boardSize: 7,
-    numShips: 3,
-    shipLength: 3,
-    shipSunk: 0,
+    boardSize: 7,  // The grid size of the board
+    numShips: 3,   // The number of ships on the play board
+    shipLength: 3, // Then length of the ship
+    shipSunk: 0,   // Counts the number of ships that are sunk
 
     ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
             { locations: ["24", "34", "44"], hits: ["", "", ""] },
@@ -30,18 +30,51 @@ var model = {
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
             var ship = this.ships[i];
-            var locations = ship.locations;
-            var index = locations.indexOf(guess);
+            var index = ship.locations.indexOf(guess);
             if (index >= 0) {
                 ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    this.shipSunk++;
+                    view.displayMessage("You sank my battleship!");
+                }
                 return true;
             }
         }
+        view.displayMiss(guess);
+        view.displayMessage("You missed...");
         return false;
+    },
+
+    isSunk: function(ship) {
+        for (var i = 0; i <= this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
-// view.displayHit("00");
-// view.displayMiss("01");
-// view.displayMiss("02");
-// view.displayMessage("HI THERE!");
+var controller = {
+    guesses: 0,
+
+    processGuess: function(guess) {
+        // DO STUFF
+    }
+};
+
+model.fire("53");
+
+model.fire("06");
+model.fire("16");
+model.fire("26");
+
+model.fire("34");
+model.fire("24");
+model.fire("44");
+
+model.fire("12");
+model.fire("11");
+model.fire("10");
