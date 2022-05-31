7d563238ca1f4dc733ef2d76cb20b2f3b5c35c4d
Eddie Gonzalez <eddie_gonzalez@Eddies-MBP.fios-router.home>
2022-05-30 22:38:08 -0700

    added battleship_game

---
+++ battleship_game.js
@@ -0,0 +1,23 @@
+
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messagearea");
+		messageArea.innerHTML = msg;
+	},
+
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
+	}
+
+}; 
+
+view.displayHit("00");
+view.displayMiss("01");
+view.displayMiss("02");
+view.displayMessage("HI THERE!");
