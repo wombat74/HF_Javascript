7cbb3e10058e154b07d97a14a6e9a75c4b1d44cb
Eddie Gonzalez <eddie_gonzalez@Eddies-MBP.fios-router.home>
2022-06-02 17:12:02 -0700

    final updates to battleship

--- battleship_game.js
+++ battleship_game.js
@@ -22,9 +22,9 @@ var model = {
     shipLength: 3, // Then length of the ship
     shipSunk: 0,   // Counts the number of ships that are sunk
 
-    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-            { locations: ["24", "34", "44"], hits: ["", "", ""] },
-            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    ships: [{ locations: [0, 0, 0], hits: ["", "", ""] },
+            { locations: [0, 0, 0], hits: ["", "", ""] },
+            { locations: [0, 0, 0], hits: ["", "", ""] }],
 
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
@@ -55,6 +55,51 @@ var model = {
             }
         }
         return true;
+    },
+
+    generateShipLocations: function() {
+        var locations;
+        for (var i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip(); // generate new locations
+            } while (this.collision(locations)); // while locations overlap keep looping (trying)
+            this.ships[i].locations = locations; // when no overlap assign location to ships array
+        }
+    },
+
+    generateShip: function() {
+        var direction = Math.floor(Math.random() * 2);
+        var row;
+        var col;
+        if (direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+
+        var newShipLocations = [];
+        for (var i = 0; i < this.shipLength; i++) {
+            if (direction === 1) {
+                newShipLocations.push(row + "" + (col + i));
+            } else {
+                newShipLocations.push((row + i) + "" + col);
+            }
+        }
+        return newShipLocations;
+    },
+
+    collision: function(locations) {
+        for (var i = 0; i < this.numShips; i++) { // for each ship already on the board
+            var ship = this.ships[i];
+            for (var j = 0; j < locations.length; j++) { // check for locations on new location array overlaping with any existing locations
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
 
@@ -65,6 +110,7 @@ var controller = {
         var location = parseGuess(guess); // should this be this.parseGuess(location)?
         if (location) {
             this.guesses++;
+            console.log("guesses " + this.guesses); // testing
             var hit = model.fire(location);
             if (hit && model.shipSunk === model.numShips) {
                 view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses!");
@@ -98,6 +144,8 @@ function init() {
     fireButton.onclick = handleFireButton;
     var guessInput = document.getElementById("guessinput");
     guessInput.onkeydown = handleKeyPress;
+
+    model.generateShipLocations();
 }
 
 function handleKeyPress(e) {
